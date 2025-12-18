964aa627b042d2ca80eb60285863750a809409ff
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-12-18 13:23:00 -0600

    added models to django admin

--- enhanced_beauty_api/admin.py
+++ enhanced_beauty_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Service, Client, Visit, VisitService
 
-# Register your models here.
+admin.site.register(Service)
+admin.site.register(Client)
+admin.site.register(Visit)
+admin.site.register(VisitService)
