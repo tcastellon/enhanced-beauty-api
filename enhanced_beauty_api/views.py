680f2309db6a846c7e99b5a2c76c7a0fa61e2a88
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-12-18 15:15:14 -0600

    added viewsets for the endpoints

--- enhanced_beauty_api/views.py
+++ enhanced_beauty_api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from .models import Client, Service, Visit, VisitService
+from .serializers import ClientSerializers, ServiceSerializer, VisitSerializer, VisitServiceSerializer
 
-# Create your views here.
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializers
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+
+class VisitViewSet(viewsets.ModelViewSet):
+    queryset = Visit.objects.all()
+    serializer_class = VisitSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+
+class VisitServiceViewSet(viewsets.ModelViewSet):
+    queryset = VisitService.objects.all()
+    serializer_class = VisitServiceSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
