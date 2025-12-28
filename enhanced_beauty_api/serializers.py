cd126dc41550b7639ff256d0b930ed79bdb6b67f
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-12-28 15:43:07 -0600

    added read_only fields to Client, Visit, and VisitService serializers

--- enhanced_beauty_api/serializers.py
+++ enhanced_beauty_api/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from .models import Client, Service, Visit, VisitService
 
-class ClientSerializers(serializers.ModelSerializer):
+class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = '__all__'
+        read_only_fields = ['created_by_user', 'created_at', 'updated_at']
 
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,8 +16,10 @@ class VisitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Visit
         fields = '__all__'
+        read_only_fields = ['created_by_user', 'created_at', 'updated_at']
 
 class VisitServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = VisitService
         fields = '__all__'
+        read_only_fields = ['created_at']
