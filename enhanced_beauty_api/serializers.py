e0ccf9ece8bf16d800420afcbc233eea97f182b6
Thomas Robert Castellon <thomas.castellon@gmail.com>
2026-01-09 13:58:09 -0600

    updated serializers

--- enhanced_beauty_api/serializers.py
+++ enhanced_beauty_api/serializers.py
@@ -1,25 +1,31 @@
 from rest_framework import serializers
 from .models import Client, Service, Visit, VisitService
 
+
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = '__all__'
-        read_only_fields = ['created_by_user', 'created_at', 'updated_at']
+        fields = "__all__"
+        read_only_fields = ["created_by_user", "created_at", "updated_at"]
+
 
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
-        fields = '__all__'
+        fields = "__all__"
+
 
 class VisitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Visit
-        fields = '__all__'
-        read_only_fields = ['created_by_user', 'created_at', 'updated_at']
+        fields = "__all__"
+        read_only_fields = ["created_by_user", "created_at", "updated_at"]
+
 
 class VisitServiceSerializer(serializers.ModelSerializer):
+    service_name = serializers.CharField(source="service.name", read_only=True)
+
     class Meta:
         model = VisitService
-        fields = '__all__'
-        read_only_fields = ['created_at']
+        fields = ['id', 'visit', 'service', 'service_name', 'created_at']
+        read_only_fields = ["created_at"]
