14a365ce5ed9d8b593484660863e0b1fcd4b7810
Thomas Robert Castellon <thomas.castellon@gmail.com>
2026-01-20 16:04:52 -0600

    updated return response for login

--- enhanced_beauty_api/views.py
+++ enhanced_beauty_api/views.py
@@ -14,34 +14,42 @@ from .serializers import (
     VisitServiceSerializer,
 )
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 @permission_classes([AllowAny])
 def login(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
+    username = request.data.get("username")
+    password = request.data.get("password")
 
     if not username or not password:
         return Response(
-            {'detail': 'Username and password are required.'},
-            status = status.HTTP_400_BAD_REQUEST
+            {"detail": "Username and password are required."},
+            status=status.HTTP_400_BAD_REQUEST,
         )
 
     user = authenticate(username=username, password=password)
 
     if user is None:
         return Response(
-            {'detail': 'Invalid credentials'},
-            status = status.HTTP_400_BAD_REQUEST
+            {"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
         )
 
     user.last_login = timezone.now()
     user.save()
 
-    token, created = Token.objects.get_or_create(user=user)
+    token, _ = Token.objects.get_or_create(user=user)
 
-    return Response({
-        'token': token.key
-    })
+    return Response(
+        {
+            "token": token.key,
+            "user": {
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "username": user.username,
+            },
+        }
+    )
+
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
@@ -82,7 +90,7 @@ def register(request):
         user.is_staff = True
         user.save()
 
-        token, created = Token.objects.get_or_create(user=user)
+        token, _ = Token.objects.get_or_create(user=user)
 
         return Response(
             {
