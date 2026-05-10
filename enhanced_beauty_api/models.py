3cc04e4603c6a1efb6ba999ff2896207bd095202
Thomas Robert Castellon <thomas.castellon@gmail.com>
2026-05-09 20:57:55 -0500

    added models to support booking

--- enhanced_beauty_api/models.py
+++ enhanced_beauty_api/models.py
@@ -1,11 +1,50 @@
+import random
+import string
 from django.db import models
 from django.contrib.auth.models import User
 
+def _generate_booking_reference():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
 class Service(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    duration = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
+
+class Availability(models.Model):
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    booked = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['date', 'start_time']
+
+    def __str__(self):
+        return f"{self.date} - {self.start_time}"
+
+class Booking(models.Model):
+    STATUS_CHOICES = [
+        ('confirmed', 'Confirmed'),
+        ('cancelled', 'Cancelled'),
+        ('rescheduled', 'Rescheduled')
+    ]
+
+    availability = models.OneToOneField(Availability, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    reference = models.CharField(default=_generate_booking_reference, max_length=8, unique=True)
+    status = models.CharField(choices=STATUS_CHOICES, default='confirmed', max_length=11)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.availability.date} - {self.availability.start_time}"
 
 class Client(models.Model):
     created_by_user = models.ForeignKey(User, on_delete=models.CASCADE)
