9c0954caed23623b8cc74037edd81a4f872cfc42
Thomas Robert Castellon <thomas.castellon@gmail.com>
2026-05-09 21:37:24 -0500

    registered Booking and Availability in admin

--- enhanced_beauty_api/admin.py
+++ enhanced_beauty_api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Service, Client, Visit, VisitService
+from .models import Service, Client, Visit, VisitService, Booking, Availability
 
 admin.site.register(Service)
 admin.site.register(Client)
 admin.site.register(Visit)
 admin.site.register(VisitService)
+admin.site.register(Booking)
+admin.site.register(Availability)
